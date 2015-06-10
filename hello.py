ba68b0646df862146b1a24771834c08d8ba926a6
colsonma <colsonma@gmail.com>
2015-06-10 16:33:00 -0400

    Adding Redis
    Added Import Redis and set req

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import os
 import uuid
+import Redis
 from flask import Flask
 app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
@@ -25,3 +26,9 @@ def hello():
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
+
+
+rediscloud_service = json.loads(os.environ['VCAP_SERVICES'])['rediscloud'][0]
+credentials = rediscloud_service['credentials']
+r = redis.Redis(host=credentials['hostname'], port=credentials['port'], password=credentials['password'])
+r.set("hit_counter", 1)
