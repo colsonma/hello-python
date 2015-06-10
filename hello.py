4123ecb763ad34c5a8fadee8bd08dc4b8229ac07
colsonma <colsonma@gmail.com>
2015-06-10 16:43:44 -0400

    Changed code sequence
    Changed the HTML format, and moved the r.get and r.inc

--- hello.py
+++ hello.py
@@ -13,25 +13,22 @@ rediscloud_service = json.loads(os.environ['VCAP_SERVICES'])['rediscloud'][0]
 credentials = rediscloud_service['credentials']
 r = redis.Redis(host=credentials['hostname'], port=credentials['port'], password=credentials['password'])
 r.set("hit_counter", 1)
-r.inc("hit_counter")
-r.get("hit_counter")
 
 @app.route('/')
 def hello():
-
+ r.inc("hit_counter")
+ 
 	return """
 	<html>
 	<body bgcolor="{}">
 
 	<center><h1><font color="white">Hi, I'm GUID:<br/>
-	{}
-	</center>
-        <Center><h1><font color="black">Hit Counter:<br/>
-	{}
+	<center><h1><font color="black">Hit Counter:<br/>
+	{}{}
 	</center>
 	</body>
 	</html>
-	""".format(COLOR,my_uuid)
+	""".format(COLOR,my_uuid,r.get("hit_counter"))
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
