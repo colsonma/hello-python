eeb612751c9289bba033df1d1338c455e0a56e1e
colsonma <colsonma@gmail.com>
2015-06-12 13:50:23 -0400

    Changing Stupid Twitter Code
    Removed Tkinter and fixed syntax

--- hello.py
+++ hello.py
@@ -20,11 +20,12 @@ credentials = rediscloud_service['credentials']
 r = redis.Redis(host=credentials['hostname'], port=credentials['port'], password=credentials['password'])
 r.set("hit_counter", 1)
 
-api = twitter.Api(['Twitter'])
+api = twitter.Api(
  access_consumer_key = os.environ['access_consumer_key'],
  access_consumer_secret = os.environ['access_consumer_secret']
  access_token_key = os.environ['access_token_key']
  access_token_secret = os.environ['access_token_secret']
+)
 
 search = api.GetSearch(
     term=raw_input("Search Term"),
@@ -39,53 +40,6 @@ for t in search:
     #Add the .encode to force encoding
     print t.text.encode('utf-8')
     print ''
-
-#attempt at adding a GUI
-    
-class simpleapp_tk(Tkinter):
-    def __init__(self,parent):
-        Tkinter.__init__(self,parent)
-        self.parent = parent
-        self.initialize()
-        
-    def initialize(self):
-        self.grid()
-        
-        self.entryVariable = Tkinter.StringVar()
-        self.entry = Tkinter.Entry(self,textvariable=self.entryVariable)
-        self.entry.grid(column=0,row=0,sticky='EW')
-        self.entry.bind("<Return>", self.OnPressEnter)
-        self.entryVariable.set(u"Enter text here.")
-        
-        button = Tkinter.Button(self,text=u"Click me!",
-                                command=self.OnButtonClick)
-        button.grid(column=1,row=0)
-        
-        self.labelVariable = Tkinter.StringVar()
-        label = Tkinter.Label(self,
-                              anchor="w",fg="white",bg="blue")
-        label.grid(column=0,row=1,columnspan=2,sticky='EW')
-        self.labelVariable.set(u"Hello!")
-        
-        self.grid_columnconfigure(0,weight=1)
-        self.resizable(True,False)
-        self.update()
-        self.geometry(self.geometry())
-        self.entry.focus_set()
-        self.entry.selection_range(0, Tkinter.END)
-          
-    
-def OnButtonClick(self):
-    self.labelVariable.set( self.entryVariable.get()+" (You clicked the button!)")
-    self.entry.focus_set()
-    self.entry.selection_range(0, Tkinter.END)
-    
-def OnPressEnter(self,event):
-    self.labelVariable.set( self.entryVariable.get()+" (You Pressed Enter!)")
-    self.entry.focus_set()
-    self.entry.selction_range(0, Tkinter.END)
-    
-
 
 @app.route('/')
 def hello():
@@ -105,7 +59,7 @@ def hello():
 
 	</body>
 	</html>
-	""".format(COLOR,my_uuid,r.get("hit_counter"))
+	""".format(COLOR,my_uuid,r.get("hit_counter"),search("Search Term"))
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
